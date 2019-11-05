48729d4d0e67d2d053deba3b319b7de1c15c0f73
Nicola Leccisi <nickleccisi1993@gmail.com>
2019-11-06 00:12:14 +0100

    modificato PDF

--- src/services/pdf/pdf.class.js
+++ src/services/pdf/pdf.class.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const moment = require('moment');
+const pdfhtml = require('html-pdf');
 
 /* eslint-disable no-unused-vars */
 exports.Pdf = class Pdf {
@@ -17,64 +18,30 @@ exports.Pdf = class Pdf {
   }
 
   async get(id, params) {
-    // pdf.create(html, { format: 'Letter' }).toStream(function(err, stream) {
-    //   if (err) {
-    //     res.json({
-    //       message: 'Sorry, we were unable to generate pdf'
-    //     });
-    //   }
-
-    //   stream.pipe(res); // your response
-    // });
-    try{
-      const student = this.app.service('certificates');
-      const tmp = await student.get(1,{query:{$eager:'student'}});
-      //const debug = await student.find();
-      console.log(tmp);
-    }catch(e){
-      console.log(id);
-      console.log(e);
-    }
+ 
+    const certificates = this.app.service('certificates');
+    const partnerService = this.app.service('partner');
+    const result = await certificates.get(id,{query:{$eager:'student'}});
+    const student = result.student;
+    const partner = await partnerService.get(student.idPartner);
+    let partnerLogo = partner.logoLink;
 
     let file = this.html;
     let now = moment().format('DD/MM/YYYY');
-    //let partnerLogo = '<img style="margin-left: 10px; margin-top: 15px;" height="150" width="250" src="https://gecolife.files.wordpress.com/2019/03/picimagine.jpg?w=620"></img>';
-    let partnerLogo = null;
-
-    file = file.replace('$NOME', 'ENRICO');
-    file = file.replace('$COGNOME', 'DITTO');
-    file = file.replace('$CITTA', 'NAPOLI');
-    file = file.replace('$DATA', '05/01/2019');
-    file = file.replace('$PROTOCOLLO', '123456');
-    file = file.replace('$DATARILASCIO', now);
+    
+    file = file.replace('$NOME', student.nome.toUpperCase());
+    file = file.replace('$COGNOME', student.cognome.toUpperCase());
+    file = file.replace('$CITTA', student.comuneNascita.toUpperCase());
+    file = file.replace('$DATA', moment(student.dataNascita).format('DD/MM/YYYY'));
+    file = file.replace('$PROTOCOLLO', result.number);
+    file = file.replace('$DATARILASCIO', moment(result.createdAt).format('DD/MM/YYYY'));
+    file = file.replace('$datasuperamento', moment(result.createdAt).format('DD/MM/YYYY'));
     file = file.replace('XXXXXXX', now);
-
-    if(partnerLogo!=null){
-      file = file.replace('$PARTNERLOGO',partnerLogo);
+    if(partnerLogo!=null){ 
+      file = file.replace('$PARTNERLOGO','<img style="margin-left: 10px; margin-top: 15px;" height="150" width="250" src="'+partnerLogo+'"></img>');
     }else{
       file = file.replace('$PARTNERLOGO','<img style="margin-left: 10px; margin-top: 15px;" height="150" width="250" src="http://localhost:3030/background/default.jpg"></img>');
     }
-
     return file;
   }
-
-  // async create (data, params) {
-  //   if (Array.isArray(data)) {
-  //     return Promise.all(data.map(current => this.create(current, params)));
-  //   }
-
-  //   return data;
-  // }
-
-  // async update (id, data, params) {
-  //   return data;
-  // }
-
-  // async patch (id, data, params) {
-  //   return data;
-  // }
-
-  // async remove (id, params) {
-  //   return { id };
-  // }
 };
