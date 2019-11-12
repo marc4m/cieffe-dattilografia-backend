7c1f961bb5050c4656a4c833bc1784b619bf709e
Roland Varriale <rolesktop>
2019-11-12 23:46:35 +0100

    aggiunta conversione immagini in base64 per le slide

--- src/services/modules/modules.class.js
+++ src/services/modules/modules.class.js
@@ -20,4 +20,26 @@ LEFT JOIN students_modules sm ON (
 WHERE m.enabled = 1
 
   */
+
+  convertImageToBase64(slide) {
+    const { image } = slide;
+
+    if (!image) return slide;
+
+    let imageBase64 = image.toString('base64');
+
+    return { ...slide, image: imageBase64 };
+  }
+
+  async get(id, params) {
+    const result = await super.get(id, params);
+
+    const { slides } = result;
+
+    if (!slides) return result;
+
+    const slidesConverted = slides.map(this.convertImageToBase64);
+
+    return { ...result, slides: slidesConverted };
+  }
 };
--- src/services/slides/slides.class.js
+++ src/services/slides/slides.class.js
@@ -9,4 +9,23 @@ exports.Slides = class Slides extends Service {
       model: Model
     });
   }
+
+  // Creo l'oggetto quiz
+  async find(params) {
+    const result = await super.find(params);
+
+    const imagesToBase64 = result.map(item => {
+      const { image } = item;
+
+      if (!image) return item;
+
+      //const imageToBase64 = ;
+
+      console.log(image);
+
+      return { ...item, image: null };
+    });
+
+    return imagesToBase64;
+  }
 };
