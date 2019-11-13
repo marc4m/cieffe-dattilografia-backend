ed171518e0db36e868093f9abb8c102249e48740
Marcello <marcelloquattromani@gmail.com>
2019-11-14 00:51:05 +0100

    fix certificates backend

--- src/services/generator/generator.class.js
+++ src/services/generator/generator.class.js
@@ -7,18 +7,30 @@ exports.Generator = class Generator {
   async create (data, params) {
 
     const certificatesServ = this.app.service('certificates');
+    const modulesServ = this.app.service('modules');
+    
 
     try {
       // Creo il certificato
       const certificateResult = await certificatesServ.create(data, params);
 
       // Creo i moduli fake
+      const mod = {
+        students: [
+          {
+            idUtente: data.idStudent,
+            corretAnswers: 10,
+            incorrectAnswers: 0
+          }
+        ]
+      };
+      await Promise.all([modulesServ.patch(1,mod),modulesServ.patch(2,mod),modulesServ.patch(3,mod)]);
 
       // Creo le risposte fake
 
       return certificateResult;
     } catch(e){
-      console.log(e);
+      //console.log(e);
     }
   }
 };
