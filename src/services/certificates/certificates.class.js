489ffc07ea8255b96e2e7b2fcc8a88fb53be724c
Marcello <marcelloquattromani@gmail.com>
2019-11-16 19:38:50 +0100

    fix crediti partner quando crea studente e rimosso quando crea attestato

--- src/services/certificates/certificates.class.js
+++ src/services/certificates/certificates.class.js
@@ -14,27 +14,27 @@ exports.Certificates = class Certificates extends Service {
     this.app = app;
   }
 
-  async removeCredit(user) {
-    const { id, credits } = user;
+  // async removeCredit(user) {
+  //   const { id, credits } = user;
 
-    if (user.credits <= 0) {
-      throw new Error('Non hai abbastanza crediti');
-    }
+  //   if (user.credits <= 0) {
+  //     throw new Error('Non hai abbastanza crediti');
+  //   }
 
-    const userService = this.app.service('users');
+  //   const userService = this.app.service('users');
 
-    return await userService.patch(id, { credits: credits - 1 });
-  }
+  //   return await userService.patch(id, { credits: credits - 1 });
+  // }
 
-  async patch(id, data, params) {
-    const { user, permitted } = params;
-    const { enabled } = data;
+  // async patch(id, data, params) {
+  //   const { user, permitted } = params;
+  //   const { enabled } = data;
 
-    // Non sono admin quindi controllo e rimuovo crediti
-    if (!permitted && enabled) await this.removeCredit(user);
+  //   // Non sono admin quindi controllo e rimuovo crediti
+  //   if (!permitted && enabled) await this.removeCredit(user);
 
-    const result = await super.patch(id, data, params);
+  //   const result = await super.patch(id, data, params);
 
-    return result;
-  }
+  //   return result;
+  // }
 };
--- src/services/generator/generator.class.js
+++ src/services/generator/generator.class.js
@@ -1,14 +1,12 @@
 /* eslint-disable no-unused-vars */
 exports.Generator = class Generator {
-  setup(app){
+  setup(app) {
     this.app = app;
   }
 
-  async create (data, params) {
-
+  async create(data, params) {
     const certificatesServ = this.app.service('certificates');
     const modulesServ = this.app.service('modules');
-    
 
     try {
       // Creo il certificato
@@ -24,12 +22,16 @@ exports.Generator = class Generator {
           }
         ]
       };
-      await Promise.all([modulesServ.patch(1,mod),modulesServ.patch(2,mod),modulesServ.patch(3,mod)]);
+      await Promise.all([
+        modulesServ.patch(1, mod),
+        modulesServ.patch(2, mod),
+        modulesServ.patch(3, mod)
+      ]);
 
       // Creo le risposte fake
 
       return certificateResult;
-    } catch(e){
+    } catch (e) {
       //console.log(e);
     }
   }
